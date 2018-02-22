2ccaed8ed3d4ccdf044f1a9645b8820f44060830
littleghostprince <amejia@student.neumont.edu>
2018-02-22 11:20:23 -0700

    EY BOY

--- src/main/java/edu/neumont/csc150/Screen/StartScreen.java
+++ src/main/java/edu/neumont/csc150/Screen/StartScreen.java
@@ -2,14 +2,20 @@ package edu.neumont.csc150.Screen;
 
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 
 public class StartScreen {
+    @FXML
+    Button button;
 
     @FXML
     public void tutorialClick(MouseEvent event) throws Exception {
 
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
         Tutorial tutorial = new Tutorial();
 
         tutorial.start(Tutorial.tutstage);
