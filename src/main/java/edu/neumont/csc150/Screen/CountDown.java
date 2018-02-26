7db9f2c30d5ea73bdc1302bbb51bbdf1fa495084
snowyorb <mabrego@student.neumont.edu>
2018-02-26 13:46:01 -0700

    HA BA BA BA

--- src/main/java/edu/neumont/csc150/Screen/CountDown.java
+++ src/main/java/edu/neumont/csc150/Screen/CountDown.java
@@ -1,15 +1,37 @@
 package edu.neumont.csc150.Screen;
 
 import javafx.application.Application;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class CountDown extends Application {
+    private boolean poison;
+    private int seconds;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    public int getSeconds() {
+        return seconds;
+    }
 
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+
+
+    public boolean isPoison() {
+        return poison;
+    }
+
+    public void setPoison(boolean poison) {
+        this.poison = poison;
+    }
+
+
+
+    public void start(Stage stage) throws Exception {
+        HBox hbox = new HBox();
     }
 }
