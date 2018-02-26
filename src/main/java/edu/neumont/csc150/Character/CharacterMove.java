f38d59c91e27bf603f1b22e8a50610cfc7aa88ef
littleghostprince <amejia@student.neumont.edu>
2018-02-26 16:35:25 -0700

    EY BOY

--- src/main/java/edu/neumont/csc150/Character/CharacterMove.java
+++ src/main/java/edu/neumont/csc150/Character/CharacterMove.java
@@ -1,10 +1,12 @@
 package edu.neumont.csc150.Character;
 
+import com.sun.webkit.dom.KeyboardEventImpl;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
@@ -31,13 +33,29 @@ public class CharacterMove {
 
         gc.fillOval(ballx, bally, ballwidth, ballheight);
 
+        ballx +=vX;
+        bally+=vY;
+
+
 
     }
 
 
     public void handle(KeyEvent e) {
 
-        if(e.getEventType() == KeyEvent. )
+        if(e.getCode() == KeyCode.S ){
+            vY+=1;
+            System.out.println("pressed");
+        }
+        if(e.getCode().equals(KeyCode.DOWN)){
+            vY-=1;
+        }
+        if(e.getCode().equals(KeyCode.LEFT)){
+            vX-=1;
+        }
+        if(e.getCode().equals(KeyCode.RIGHT)){
+            vX+=1;
+        }
 
     }
 }
