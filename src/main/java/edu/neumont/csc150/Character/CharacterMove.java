448f5f5a1d3402e3983a1d13bc3487faeb7e5fa4
littleghostprince <amejia@student.neumont.edu>
2018-02-28 09:16:55 -0700

    EY BOY

--- src/main/java/edu/neumont/csc150/Character/CharacterMove.java
+++ src/main/java/edu/neumont/csc150/Character/CharacterMove.java
@@ -24,7 +24,7 @@ public class CharacterMove implements EventHandler <KeyEvent> {
     @FXML
     private Canvas mainCanvas;
 
-    private int ballx = 460,bally = 560,ballwidth =50,ballheight=50;
+    private int ballx = 460,bally = 540,ballwidth =50,ballheight=50;
 
     private int vX = 20 , vY = 20;
 
@@ -63,35 +63,52 @@ public class CharacterMove implements EventHandler <KeyEvent> {
 
     }
 
-    public void checkbounce(){
+    public boolean checkbounce(){
         if(bally + ballheight >= this.mainCanvas.getHeight() || bally <=0){
-          //  vY=0;
+            vY*=-1;
+            return true;
         }
         if(ballx + ballwidth >= this.mainCanvas.getWidth() || ballx <= 0){
-           // vX =0;
+            vX *=-1;
+            return true;
         }
+        return false;
 
     }
     @Override
     public void handle(KeyEvent e) {
             if(e.getCode().equals(KeyCode.UP) || e.getCode().equals(KeyCode.W)) {
                 System.out.println("UP key was pressed");
-                bally -=vY;
+
                 this.checkbounce();
+                bally -=vY;
+
             }
             if(e.getCode().equals(KeyCode.DOWN)|| e.getCode().equals(KeyCode.S)){
                 System.out.println("Down key was pressed");
+
+                this.checkbounce();
                 bally +=vY;
+
             }
              if(e.getCode().equals(KeyCode.LEFT) || e.getCode().equals(KeyCode.A)){
             System.out.println("Left key was pressed");
+
+                this.checkbounce();
                 ballx-=vX;
+
             }
              if(e.getCode().equals(KeyCode.RIGHT) || e.getCode().equals(KeyCode.D)){
             System.out.println("Right key was pressed");
+
+                this.checkbounce();
                 ballx+=vX;
+
             }
 
+        if(e.getCode().equals(KeyCode.SPACE) || e.getCode().equals(KeyCode.D)) {
+            System.out.println("Space key was pressed");
+        }
 
 
     }
